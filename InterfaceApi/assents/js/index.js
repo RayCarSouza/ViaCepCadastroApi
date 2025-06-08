98e104f8598ae9768019afd22639c2d28f8f21fc
Rayra Carvalho de Souza <166947300+RayCarSouza@users.noreply.github.com>
2025-06-08 19:45:22 -0300

    Projeto Senai consumindo Api usando ViaCep

---
+++ InterfaceApi/assents/js/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function preencherFormulario(endereco) {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const cepValido = cep => cep.length === 8;
+
+const pesquisarCep = async () => {
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        } else {
+            preencherFormulario(endereco);
+        }
+    } else {
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+};
